e32ee7d9d39dd167d9c076f967d5ad5f3801dd29
Andrea De Vita <adevita@lxplus916.cern.ch>
2024-09-10 13:19:09 +0200

    REMOVE debugging file

--- model_check.py
+++
@@ -1,9 +0,0 @@
-import onnx
-from onnx import checker
-
-# Carica il modello
-model = onnx.load("/afs/cern.ch/user/a/adevita/public/workDir/k4RecTracker/Tracking/model_multivector_1_input.onnx")
-
-# Verifica il modello
-checker.check_model(model)
-print("Modello ONNX valido")
